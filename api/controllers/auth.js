7dd49c3ce8a8733e39cb8ef497d045e18a2be146
Kushan Nalinka <kushannalinka572@gmail.com>
2024-02-04 17:28:38 +0530

    Modifications of hotels,rooms,users
    Add new modifications

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -10,8 +10,9 @@ export const register = async (req, res, next) => {
   
     const newUser = new User({
       
-        username:req.body.username,
-        email:req.body.email,
+       // username:req.body.username,
+       // email:req.body.email,
+       ...req.body,
         password:hash,
 
     });
